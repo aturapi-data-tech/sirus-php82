fc91abdc45f9e87c7aa9fd078bbdb6da76a73b31
SYAMS. info <nuurwahidanshary@gmail.com>
2026-02-13 14:16:46 +0700

    fix(check box):component di update biar inline

--- resources/views/components/check-box.blade.php
+++ resources/views/components/check-box.blade.php
@@ -15,7 +15,7 @@
 @endphp
 
 <div {{ $attributes->only(['class']) }}>
-    <div class="flex items-center h-5">
+    <div class="flex items-center">
         <input type="checkbox" id="{{ $id }}" name="{{ $name ?? $id }}" value="{{ $value }}"
             @if ($checked) checked @endif @if ($disabled) disabled @endif
             @if ($required) required @endif
@@ -24,24 +24,22 @@
                     'w-4 h-4 text-brand-green bg-gray-100 border-gray-300 rounded focus:ring-brand-green/50 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 ' .
                     ($error ? 'border-red-500 dark:border-red-500' : ''),
             ]) }}>
-    </div>
 
-    @if ($label)
-        <div class="ml-3 text-sm">
+        @if ($label)
             <label for="{{ $id }}"
-                class="font-medium text-gray-700 dark:text-gray-300 cursor-pointer {{ $disabled ? 'text-gray-400 dark:text-gray-500 cursor-not-allowed' : '' }}">
+                class="ml-2 text-sm font-medium text-gray-700 dark:text-gray-300 cursor-pointer {{ $disabled ? 'text-gray-400 dark:text-gray-500 cursor-not-allowed' : '' }}">
                 {{ $label }}
 
                 @if ($required)
                     <span class="text-red-500">*</span>
                 @endif
             </label>
+        @endif
+    </div>
 
-            @if ($helpText)
-                <p class="mt-1 text-gray-500 dark:text-gray-400">
-                    {{ $helpText }}
-                </p>
-            @endif
-        </div>
+    @if ($helpText)
+        <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
+            {{ $helpText }}
+        </p>
     @endif
 </div>
